a2caca08a8abfc4a99e37b27ad5e75d121cea4a8
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-06-11 11:49:37 +0700

    EPISODE 10 METHOD AND RETURN TYPES

--- EPISODE 10 METHOD AN RETURN TYPES/src/com/raini/Main.java
+++ EPISODE 10 METHOD AN RETURN TYPES/src/com/raini/Main.java
@@ -3,6 +3,51 @@ package com.raini;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // write your code here
+        myName("RANDY");
+
+        int finalResult = addNums(4, 5);
+
+        System.out.println( finalResult);
+
+
+        System.out.println( fullName("Randy", "Ramadhan"));
+
+        System.out.println(showChar('D'));
+
+
     }
+
+    //Method
+    public static void myName(String mName) {
+
+        System.out.println(mName);
+
+    }
+
+    // Return an integer
+
+    public static int addNums(int a, int b) {
+        int result;
+        result = a + b;
+        //  System.out.println(" Sum = "  + (a + b));
+
+        return result ; // (a+b)
+    }
+
+    //Return a string
+
+    public static String fullName(String firstName, String lastName){
+
+        return firstName + " " + lastName;
+    }
+
+    // Return Character
+    public static char showChar(char c) {
+        return c;
+    }
+
+
+
 }
