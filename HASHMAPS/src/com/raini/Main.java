f56b352e56a10a3f0b4f5e282fd2fa5a48b58add
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-06-11 14:21:56 +0700

    HASHMAPS

--- HASHMAPS/src/com/raini/Main.java
+++ HASHMAPS/src/com/raini/Main.java
@@ -1,8 +1,32 @@
 package com.raini;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Hashmaps (key --> value)
+
+        HashMap<Integer, String> newUsers = new HashMap<>();
+        newUsers.put(23, "Sandra");
+
+        HashMap users = new HashMap();
+        users.put("Sandra", 23);
+        users.put("Paul" , 31);
+        users.put("George" , 74);
+        users.remove("Sandra");
+
+        // System.out.println(users.get("Paul"));
+
+        Iterator iterator = users.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry pair = (Map.Entry) iterator.next();
+            System.out.println("Key: " + pair.getKey() + " Value: " + pair.getValue());
+        }
+
+        // System.out.println(users.entrySet());
+
     }
 }
