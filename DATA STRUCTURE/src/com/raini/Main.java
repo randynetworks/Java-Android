c5b4449932966f27ed58947adddced6713578383
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-06-11 13:06:56 +0700

    CONCEPT OF ARRAY

---
+++ DATA STRUCTURE/src/com/raini/Main.java
@@ -0,0 +1,18 @@
+package com.raini;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //ARRAY DECLARATION
+        int [] myArray = {1,2, 3, 4,5,6,7};
+        String[] myStringArray = {"hello", "Hi", "what"};
+
+        // array have length.
+        for (int i = 0; i < myStringArray.length; i++){
+            System.out.println("Item " + myStringArray[i]);
+        }
+
+
+    }
+}
